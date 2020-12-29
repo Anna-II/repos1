565cbd0ba305858ec6dd687ff4c8847ccf639599
qwe5516740 <67087072+qwe5516740@users.noreply.github.com>
2020-12-29 16:11:05 +0800

    lj
    前端页面

--- vuehr/src/views/sal/SalTable.vue
+++ vuehr/src/views/sal/SalTable.vue
@@ -1,105 +1,95 @@
 <template>
-  <div>
-    <div style="display: flex">
-      <el-button type="primary">市场部</el-button>
-      <el-button type="primary">人事部</el-button>
-      <el-button type="primary">运维部</el-button>
-    </div>
-    <div style="margin-top: 10px">
-      <el-table :data="salaries" border stripe>
-        <el-table-column type="selection" width="55"></el-table-column>
-        <el-table-column
-          width="100"
-          prop="eid"
-          label="员工编号"
-        ></el-table-column>
-        <el-table-column
-          width="100"
+    <el-table
+        :data="tableData"
+        border
+        style="width: 100%">
+      <el-table-column
+          fixed
+          prop="date"
+          label="日期"
+          width="150">
+      </el-table-column>
+      <el-table-column
           prop="name"
-          label="员工姓名"
-        ></el-table-column>
-        <el-table-column
-          width="120"
-          prop="basicSalary"
-          label="基本工资"
-        ></el-table-column>
-        <el-table-column
-          width="120"
-          prop="bonus"
-          label="奖金"
-        ></el-table-column>
-        <el-table-column
-          width="80"
-          prop="lunchSalary"
-          label="午餐补助"
-        ></el-table-column>
-        <el-table-column
-          width="80"
-          prop="trafficSalary"
-          label="交通补助"
-        ></el-table-column>
-        <el-table-column
-          width="120"
-          prop="allSalary"
-          label="应发工资"
-        ></el-table-column>
-        <el-table-column label="操作">
-          <template slot-scope="scope">
-            <el-button @click="showEditSalaryView(scope.row)">编辑</el-button>
-            <el-button type="danger" @click="deleteSalary(scope.row)"
-              >删除</el-button
-            >
-          </template>
-        </el-table-column>
-      </el-table>
-    </div>
-  </div>
+          label="姓名"
+          width="120">
+      </el-table-column>
+      <el-table-column
+          prop="province"
+          label="省份"
+          width="120">
+      </el-table-column>
+      <el-table-column
+          prop="city"
+          label="市区"
+          width="120">
+      </el-table-column>
+      <el-table-column
+          prop="address"
+          label="地址"
+          width="300">
+      </el-table-column>
+      <el-table-column
+          prop="zip"
+          label="工资"
+          width="120">
+      </el-table-column>
+      <el-table-column
+          fixed="right"
+          label="操作"
+          width="100">
+        <template slot-scope="scope">
+          <el-button @click="handleClick(scope.row)" type="text" size="small">查看</el-button>
+          <el-button type="text" size="small">编辑</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
 </template>
 
 <script>
 export default {
-  name: "SalTable",
+  methods: {
+    handleClick(row) {
+      console.log(row);
+    }
+  },
+
   data() {
     return {
-      salaries: [],
-      activeItemIndex: 0,
-      salaryItemName: [
-        '基本工资',
-        '交通补助',
-        '午餐补助',
-        '奖金',
-      ],
-      salary: {
-        basicSalary: 0,
-        trafficSalary: 0,
-        lunchSalary: 0,
-        bonus: 0,
-      }
-    }
-  },
-  mounted() {
-    this.initSalaries();
-  },
-  methods: {
-    showAddSalaryView() {
-      //数据初始化
-      this.salary = {
-        basicSalary: 0,
-        trafficSalary: 0,
-        lunchSalary: 0,
-        bonus: 0,
-      }
-    },
-    initSalaries() {
-      this.getRequest("/salary/table/").then(resp => {
-        if (resp) {
-          this.salaries = resp;
-        }
-      })
+      tableData: [{
+        date: '2016-05-02',
+        name: '王小虎',
+        province: '上海',
+        city: '普陀区',
+        address: '上海市普陀区金沙江路 1518 弄',
+        zip: 200333
+      }, {
+        date: '2016-05-04',
+        name: '王小虎',
+        province: '上海',
+        city: '普陀区',
+        address: '上海市普陀区金沙江路 1517 弄',
+        zip: 200333
+      }, {
+        date: '2016-05-01',
+        name: '王小虎',
+        province: '上海',
+        city: '普陀区',
+        address: '上海市普陀区金沙江路 1519 弄',
+        zip: 200333
+      }, {
+        date: '2016-05-03',
+        name: '王小虎',
+        province: '上海',
+        city: '普陀区',
+        address: '上海市普陀区金沙江路 1516 弄',
+        zip: 200333
+      }]
     }
   }
-};
+}
 </script>
 
 <style scoped>
+
 </style>
