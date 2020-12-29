8656b467561ea75022a17b35c63858d1334a772d
qwe5516740 <67087072+qwe5516740@users.noreply.github.com>
2020-12-29 15:26:13 +0800

    l
    vh

---
+++ vhr/vhrserver/vhr-web/src/main/java/org/javaboy/vhr/controller/salary/SalaryTableController.java
@@ -0,0 +1,23 @@
+package org.javaboy.vhr.controller.salary;
+
+import org.javaboy.vhr.model.Salary;
+import org.javaboy.vhr.service.SalaryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/salary/table")
+public class SalaryTableController {
+
+    @Autowired
+    SalaryService salaryService;
+
+    @GetMapping("/")
+    public List<Salary> getSalaries() {
+        return salaryService.getSalaries();
+    }
+}
